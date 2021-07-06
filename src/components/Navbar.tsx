87a7ed23fe793b90e3e05062dec0f0d55312c43e
Joel Jacquez <jose.jacquez@coppel.com>
2021-07-05 22:39:11 -0600

    add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+
+const Navbar = () => {
+
+  return (
+    <>
+      <nav>
+        <span>
+          <Link to="/">
+            <b>Joel</b>
+            <b>Jacquez</b>
+          </Link>
+        </span>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">Resume</Link>
+          </li>
+          <li>
+            <Link to="/">Portafolio</Link>
+          </li>
+          <li>
+            <Link to="/">Blog</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar;
