8307ce2e620f3eb5ee6e6121f3fd6831ac613962
Joel Jacquez <jose.jacquez@coppel.com>
2021-07-05 22:40:03 -0600

    add Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Navbar from './Navbar';
+const Layout = (props) => {
+  const { children } = props;
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  )
+}
+
+export default Layout;
