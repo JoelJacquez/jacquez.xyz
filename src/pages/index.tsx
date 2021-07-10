4746e80da6476261a2c4fcd6ef54d42ba5e2cb04
Joel Jacquez <jose.jacquez@coppel.com>
2021-07-10 03:09:50 -0600

    make header layout of home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Layout from '../components/Layout';
-
+import { StaticImage } from 'gatsby-plugin-image';
 // styles
 
 // markup
@@ -8,14 +8,40 @@ const IndexPage = () => {
   return (
     <>
       <Layout>
-        <title>Home Page</title>
-        <h1>Welcome to my Gastby site!</h1>
-        <p>
-          I make this following the Gastby tutorial!!
-          <span role="img" aria-label="Sunglasses smiley emoji">
-            😎
-          </span>
-        </p>
+        <section className="page">
+          <div className="page-header">
+            <div className="photo-container">
+              <figure className="photo">
+                <StaticImage src="../images/photo.png" alt="" />
+              </figure>
+            </div>
+
+            <div className="author-information-container">
+              <div className="author-information">
+                <div className="fullName">Joel Jacquez</div>
+                <div className="title">Web Developer</div>
+
+                <div className="social-links">
+                  <a href="#">
+                    <i className="twitter"></i>
+                  </a>
+                  <a href="#">
+                    <i className="facebook"></i>
+                  </a>
+                  <a href="#">
+                    <i className="instagram"></i>
+                  </a>
+                  <a href="#">
+                    <i className="linkedin"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="page-content">
+            <h1>Welcome to my Gastby site!</h1>
+          </div>
+        </section>
       </Layout>
     </>
   );
