10850d74875a3a0378b4bc05af0e01f25e17cf50
Joel Jacquez <jose.jacquez@coppel.com>
2021-07-10 14:41:22 -0600

    apply basics styles to portafolio page

--- src/pages/portafolio.tsx
+++ src/pages/portafolio.tsx
@@ -4,21 +4,26 @@ import Layout from '../components/Layout';
 // styles
 
 // markup
-const IndexPage = () => {
+const PortafolioPage = () => {
   return (
     <>
       <Layout>
-        <title>Portafolio</title>
-        <h1>Portafolio</h1>
-        <p>
-          I make this following the Gastby tutorial!!
-          <span role="img" aria-label="Sunglasses smiley emoji">
-            😎
-          </span>
-        </p>
+        <section className="page">
+          <div className="page-header">
+            <h1>Portafolio</h1>
+          </div>
+          <div className="page-content">
+            <p>
+              I make this following the Gastby tutorial!!
+              <span role="img" aria-label="Sunglasses smiley emoji">
+                😎
+              </span>
+            </p>
+          </div>
+        </section>
       </Layout>
     </>
   );
 };
 
-export default IndexPage;
+export default PortafolioPage;
